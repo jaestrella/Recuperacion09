bdf17c4480128d41e83004f6fbc0bcbd55d9302d
jaestrella <jaestrella1991@gmail.com>
2018-05-25 09:15:17 +0200

    empezando actividad 5

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/recuperacion09/Actividad05.java
@@ -0,0 +1,15 @@
+package com.iesvirgendelcarmen.dam.recuperacion09;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matinal on 25/05/2018.
+ */
+
+public class Actividad05 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad05);
+    }
+}
